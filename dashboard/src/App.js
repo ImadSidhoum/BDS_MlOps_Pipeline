edea0434c0efe3746a6d338c5e2187c32ff93f96
Thomas gossin <Thomas.gossin@epita.fr>
2021-05-20 11:08:01 +0200

    dashboard

---
+++ dashboard/src/App.js
@@ -0,0 +1,13 @@
+// import {BrowserRouter, Switch, Route} from "react-router-dom"
+// import Home from "./pages/Home";
+import SiderDemo from './pages/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <SiderDemo/>
+    </div>
+  );
+}
+
+export default App;
